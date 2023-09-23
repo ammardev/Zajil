f20650002f398c20a30366c6bdd4eec19b0fb43e
Ammar Al-Khawaldeh <me@ammar.dev>
2023-09-23 14:31:48 +0300

    Create a status bar that shows the status code and TTFB

--- components/response.go
+++ components/response.go
@@ -1,6 +1,7 @@
 package components
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 
@@ -12,7 +13,12 @@ type ResponseView struct {
     textinput.Model
     Width int
     style lipgloss.Style
+    statusBarStyle lipgloss.Style
+    statusBarRightStyle lipgloss.Style
+    statusStyle lipgloss.Style
     body string
+    status int
+    ttfb int
 }
 var a lipgloss.Style
 
@@ -20,24 +26,51 @@ func NewResponseView(width int) ResponseView {
     response := ResponseView{
         Model: textinput.New(),
         style: lipgloss.NewStyle().Width(width).Border(lipgloss.RoundedBorder()),
+        statusBarStyle: lipgloss.NewStyle().
+            Width(width).
+            Padding(0, 2, 0, 2).
+            Border(lipgloss.RoundedBorder(), false, false, true, false),
+        statusStyle: lipgloss.NewStyle().
+            Width(5).
+            AlignHorizontal(lipgloss.Center).
+            Background(lipgloss.Color("42")).
+            Foreground(lipgloss.Color("232")).
+            Bold(true),
+        statusBarRightStyle: lipgloss.NewStyle().
+            Width(width - 10).
+            AlignHorizontal(lipgloss.Right),
     }
     response.Model.Prompt = ""
 
     return response
 }
 
-func (view *ResponseView) SetResponse(response http.Response) {
+func (view *ResponseView) SetResponse(response *http.Response, ttfb int) {
     body, _ := io.ReadAll(response.Body)
     view.body = string(body)
+    view.status = response.StatusCode
+    view.ttfb = ttfb
 }
 
 func (view *ResponseView) Resize(width, height int) {
     view.style.Width(width)
     view.style.Height(height)
+
+    view.statusBarStyle.Width(width)
+    view.statusBarRightStyle.Width(width-10)
 }
 
 func (view ResponseView) Render() string {
+    statusBar := view.statusBarStyle.Render("Enter a url and then press `enter` to send the request")
+    if view.status > 0 {
+        statusBar = view.statusBarStyle.Render(
+            view.statusStyle.Render(fmt.Sprint(view.status)),
+            view.statusBarRightStyle.Render(fmt.Sprint("TTFB: ", view.ttfb, "ms")),
+        )
+    }
+
     return view.style.Render(
+        statusBar,
         view.body,
     )
 }
--- request_builder.go
+++ request_builder.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"net/http"
+	"net/http/httptrace"
+	"time"
 )
 
 func sendHttpRequest(zajil *Zajil) {
@@ -10,6 +12,21 @@ func sendHttpRequest(zajil *Zajil) {
         zajil.urlInput.GetUrl(),
         nil,
     )
+
+    var startTime time.Time
+    var ttfb int
+
+    trace := &httptrace.ClientTrace{
+        DNSStart: func(httptrace.DNSStartInfo) { startTime = time.Now() },
+
+        GotFirstResponseByte: func() {
+            ttfb = int(time.Since(startTime).Milliseconds())
+        },
+    }
+
+    request = request.WithContext(httptrace.WithClientTrace(request.Context(), trace))
+
+
     res, _ := http.DefaultClient.Do(request)
-    zajil.responseView.SetResponse(*res)
+    zajil.responseView.SetResponse(res, ttfb)
 }
